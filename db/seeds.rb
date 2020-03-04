7ce331995695845437b53500baa0ed8a10401061
biagemc <biagemc@gmail.com>
2020-03-03 20:31:57 -0500

    add products to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+keyboard = Product.new(
+  name: "Mechanical Keyboard",
+  price: 95.0,
+  image_url: "",
+  description: "Cherry switch echanical keyboard with bluetooth",
+)
+keyboard.save
