4c659d0acd3b421fd219f60dff5eabca16dea1e5
biagemc <biagemc@gmail.com>
2020-03-03 20:46:08 -0500

    add display all products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,14 @@
 class Api::ProductsController < ApplicationController
+  def all_products
+    all_products = Product.all
+    @products = []
+    all_products.each do |item|
+      @products << {
+        name: item.name,
+        description: item.description,
+        price: item.price,
+      }
+    end
+    render "my_product.json.jb"
+  end
 end
