2b1b31469f0087550666a39b234f50a830503ab5
biagemc <biagemc@gmail.com>
2020-03-05 22:48:23 -0500

    add creat + update

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -9,4 +9,26 @@ class Api::ProductsController < ApplicationController
     @product = Product.find_by(id: the_id)
     render "show.json.jb"
   end
+
+  def create
+    @product = Product.new(
+      name: params[:name_input],
+      description: params[:description_input],
+      price: params[:price_input],
+      image_url: params[:image_url_input],
+    )
+    @product.save
+    render "show.json.jb"
+  end
+
+  def update
+    the_id = params[:id]
+    @product = Product.find_by(id: the_id)
+    @product.name = params[:name_input] || @product.name
+    @product.description = params[:description_input] || @product.description
+    @product.price = params[:price_input] || @product.price
+    @product.image_url = params[:image_url_input] || @product.image_url
+    @product.save
+    render "show.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   # end
 
   namespace :api do
-    get "products" => "products#index"
-    get "products/:id" => "products#show"
+    get "/products" => "products#index"
+    get "/products/:id" => "products#show"
+    post "/products" => "products#create"
+    patch "/products/:id" => "products#update"
   end
 end
