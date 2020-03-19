4f15c429c321662c68ab80bc8c8fd58169259879
biagemc <biagemc@gmail.com>
2020-03-18 20:44:02 -0400

    update create order method

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -17,17 +17,24 @@ class Api::OrdersController < ApplicationController
   end
 
   def create
+    product = Product.find_by(id: params[:product_id])
+    the_subtotal = params[:quantity].to_i * product.price
+    tax_rate = 0.09
+    the_tax = the_subtotal * tax_rate
+    the_total = the_subtotal + the_tax
+
     @order = Order.new(
       user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
+      subtotal: the_subtotal,
+      tax: the_tax,
+      total: the_total,
     )
     if @order.save
-      @order.subtotal = @order.product.price * @order.quantity
-      @order.tax = @order.subtotal * 0.09
-      @order.total = @order.subtotal + @order.tax
-      @order.save
       render "show.json.jb"
+    else
+      render json: { errors: @order.errors.full_messages }, status: :bad_request
     end
   end
 end
