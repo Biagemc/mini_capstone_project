a457ae94f4c7979ae19e242937287fe9e61d8d01
biagemc <biagemc@gmail.com>
2020-03-25 22:40:26 -0400

    add destroy action to html file

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,4 +23,30 @@ class ProductsController < ApplicationController
     @product.save
     redirect_to "/products/#{@product.id}"
   end
+
+  def edit
+    @product = Product.find(params[:id])
+    render "edit.html.erb"
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(
+      name: params[:name],
+      description: params[:description],
+      price: params[:price],
+      supplier_id: params[:supplier_id],
+    )
+    if @product.save
+      redirect_to "/product/#{@product.id}"
+    else
+      render json: { errors: @product.errros.full_messages }, status: :bad_request
+    end
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    redirect_to "/products"
+  end
 end
